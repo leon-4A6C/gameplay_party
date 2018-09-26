d9798c1246109778270a4d111a0a6e8d65f51bd6
youssra-4a6b <509717@edu.rocmn.nl>
2018-09-26 13:25:37 +0200

    changed menu

--- view/partials/head.php
+++ view/partials/head.php
@@ -8,39 +8,11 @@
     <title>Gameplay party</title>
 </head>
 <body>
-	<nav class="breadcrumb" aria-label="breadcrumbs">
+<nav class="breadcrumb is-centered has-bullet-separator" aria-label="breadcrumbs">
   <ul>
-    <li>
-      <a href="#">
-        <span class="icon is-small">
-          <i class="fas fa-home" aria-hidden="true"></i>
-        </span>
-        <span>Bulma</span>
-      </a>
-    </li>
-    <li>
-      <a href="#">
-        <span class="icon is-small">
-          <i class="fas fa-book" aria-hidden="true"></i>
-        </span>
-        <span>Documentation</span>
-      </a>
-    </li>
-    <li>
-      <a href="#">
-        <span class="icon is-small">
-          <i class="fas fa-puzzle-piece" aria-hidden="true"></i>
-        </span>
-        <span>Components</span>
-      </a>
-    </li>
-    <li class="is-active">
-      <a href="#">
-        <span class="icon is-small">
-          <i class="fas fa-thumbs-up" aria-hidden="true"></i>
-        </span>
-        <span>Breadcrumb</span>
-      </a>
-    </li>
+    <li><a href="#">Bulma</a></li>
+    <li><a href="#">Documentation</a></li>
+    <li><a href="#">Components</a></li>
+    <li><a href="#">Breadcrumb</a></li>
   </ul>
 </nav>
