de1fb40766759bb0dbb358fd8081ac96611535ac
youssra-4a6b <509717@edu.rocmn.nl>
2018-10-01 15:35:50 +0200

    bla

--- view/partials/footer.php
+++ view/partials/footer.php
@@ -3,7 +3,7 @@
     <div>
       <strong>Gameplay Party</strong> by Pieter Piper. The source code is licensed. </br> Copyright © 2018 - Gameplay Party
     </div>
-    <a href="www.facebook.nl"><img src="assets/images/facebook.png" class="button" height="40px"/></a>
+    	<a href="www.facebook.nl"><img src="assets/images/facebook.png" class="button" height="40px"/></a>
 		<a href="www.instagram.com"><img src="assets/images/instagram.png" class="button" height="45px"/></a>
   </div>
 </footer>
