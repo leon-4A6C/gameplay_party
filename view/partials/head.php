3870ed1ab6f82d0cc64a08a86b5ff63fdb85bd3f
youssra-4a6b <509717@edu.rocmn.nl>
2018-10-17 09:03:21 +0200

    add

--- view/partials/head.php
+++ view/partials/head.php
@@ -5,11 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="/css/styles.css">
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
     <title>Gameplay party</title>
 
-    <div class="logo" style="position: fixed;">
-      <img src="/view\assets\images\gpp.svg" alt="#">
-    </div>
-   </head>
-   </html>
+      <img src="\view\assets\images\gpp.svg" style="position: fixed;" alt="#">
+</head>
+</html>
